3992a258f9cba9effd4e1e673773d474c6a291d0
sushvin-research <sushvin.marimuthu@research.iiit.ac.in>
2024-05-24 15:58:12 +0530

    Route fixed.

--- main.py
+++ main.py
@@ -39,12 +39,12 @@ def is_go_installed():
         return False
 
 
-@app.get("/")
+@app.get("/api/tamilparser/server/")
 async def main():
     return {"Message": "Server is listening..."}
 
 
-@app.post("/visualizeGraph")
+@app.post("/api/tamilparser/server/visualizeGraph")
 async def get_graph(data: str = Form('data')):
     with open('data.conllu', 'w') as f:
         f.write(data)
@@ -170,7 +170,7 @@ def print_conllu_format(data):
     return result
 
 
-@app.post("/get_trankit_graph_data")
+@app.post("/api/tamilparser/server/get_trankit_graph_data")
 async def get_trankit_graph_data(data: str = Form('data')):
     data = data.replace("\n", " ")
     output = sent_tokenizer.tokenize(data.strip())
